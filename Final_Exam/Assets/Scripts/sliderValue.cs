030c66bc3ac6c6f7dcc844405069b6cdcb0e49d9
OkieHuntsman <46794724+OkieHuntsman@users.noreply.github.com>
2019-05-07 16:34:36 -0400

    Final_Exam
    Update

---
+++ Final_Exam/Assets/Scripts/sliderValue.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class sliderValue : MonoBehaviour
+{
+    public Slider mainSlider;
+
+    public float sValue = 0;
+
+    public Text slideText;
+
+    void Start()
+    {
+        slideText.text = "";
+    }
+
+    //Invoked when a submit button is clicked.
+    public void SubmitSliderSetting()
+    {
+        //Displays the value of the slider in the console.
+        sValue = mainSlider.value;
+    }
+
+    void Update()
+    {
+        slideText.text = sValue.ToString("F2");
+    }
+}
